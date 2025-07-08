2634f7e08b476ba2838a9cdab06d566da4c538d9
Soumya Ghosh <soumyaghosh825@gmail.com>
2025-07-08 23:55:59 +0530

    Initial commit for GitHub Pages

---
+++ src/Components/MyWork/Mywork_data.js
@@ -0,0 +1,33 @@
+const mywork_data = [
+  {
+    w_img: 'https://cdn.dribbble.com/users/117472/screenshots/2298243/010-day_weatherwidget.jpg?compress=1&resize=800x600',
+    title: '🌤️ Weather Web App',
+    desc: 'A responsive weather web application built using React.js that allows users to fetch and display current weather data for any city using the OpenWeatherMap API.',
+    visit: 'https://example.com/project1',
+    github: 'https://github.com/soumya897/weather_web_app'
+  },
+  {
+    w_img: 'https://0901.static.prezi.com/preview/v2/fq7ad3z6ahfg6j3kbkixizo6fx6jc3sachvcdoaizecfr3dnitcq_3_0.png',
+    title: 'Online Banking System',
+    desc: 'A Java-based console application that simulates core banking operations like account creation, deposits, withdrawals, balance checking, and transaction history. Data persistence is managed using MySQL database integration via JDBC.',
+    visit: 'https://example.com/project2',
+    github: 'https://github.com/soumya897/OnlineBankingSystem'
+  },
+  {
+    w_img: 'https://cdn.slidesharecdn.com/ss_thumbnails/enhancing-college-communications-1220307394344959-8-thumbnail.jpg?width=640&height=640&fit=bounds',
+    title: 'College to College Communication',
+    desc: 'A platform for secure communication and collaboration between multiple colleges.',
+    visit: 'https://example.com/project2',
+    github: 'https://github.com/soumya897/College-to-College-Communication'
+  },
+  {
+    w_img: 'https://tse4.mm.bing.net/th/id/OIP.yIGxgoJrFoD1QABgLRKCSgHaFj?pid=Api&P=0&h=220',
+    title: 'Number Guessing Game',
+    desc: 'A simple interactive game where users guess a randomly generated number.',
+    visit: 'https://example.com/project2',
+    github: 'https://github.com/soumya897/Number_-guessing-_game'
+  },
+  // add more...
+];
+
+export default mywork_data;
